895ffde1396276a33c0003b813c098730dd15580
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-24 19:22:18 +0100

    created post func

--- template-create.php
+++ template-create.php
@@ -6,41 +6,41 @@ Template Name: Create page
 <?php get_header(); ?>
 <section class="content-section">
     <h2>Create Post</h2>
-    <form class="" action="index.html" method="post">
+    <form class="" id='myForm' action="index.html" method="post">
+        <!-- input for title, content and status  -->
         <input type="text" name="title" id="title" placeholder="Title" class="text-input title">
         <textarea id="content" name="name" rows="8" cols="80" placeholder="Content" class="text-input"></textarea>
-        <!-- <div>
-        <input id="status" type="radio" value="publish">
-        <input id="status" type="radio" value="publish">
-        <input id="status" type="radio" value="publish">
-        </div> -->
-        <button type="submit" name="button" onclick="addPost()">Publish</button>
+        <input type="text" name="status" id="status" placeholder="status" class="text-input title">
+        <button type="submit" name="button">Publish</button>
     </form>
-    <div id="container">
-        <button id="addPost" onclick="addPost()">add post</button>
-    </div>
     <script>
-        function addPost() {
-            console.log('hello')
-            var input = {
-                'titel': document.getElementById('title').value,
-                'content': document.getElementById('content').value,
-                'status': document.getElementById('status').value
-            }
+        const myForm = document.getElementById('myForm');
+        /* When form is submited it will make a post req*/
+        myForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            /* my haeder info fo post req */
             var myHeaders = new Headers();
-            myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC93b3JkcHJlc3MubG9jYWwiLCJpYXQiOjE2NDc1MTQ5ODMsIm5iZiI6MTY0NzUxNDk4MywiZXhwIjoxNjQ4MTE5NzgzLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.ZD2QVLLJ5kN6cVFFbrLXfZcoecOYN4JsTF44NeVADic");
+            myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC93b3JkcHJlc3MubG9jYWwiLCJpYXQiOjE2NDgxMjMwOTAsIm5iZiI6MTY0ODEyMzA5MCwiZXhwIjoxNjQ4NzI3ODkwLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.haL4mOLTzCaLRXPbmW6YrUrCmbpzfalRYMIcEdY6bsU");
             myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-            var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: input,
-                redirect: 'follow'
+            /* it will take form data and create a array of it for body in requestOptions */
+            const formData = new FormData(this);
+            const input = new URLSearchParams();
+            for (const pair of formData) {
+                input.append(pair[0], pair[1])
             };
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: input,
+            redirect: 'follow'
+            };
+            /* fetch the post req to wp server */
             fetch("http://wordpress.local/wp-json/wp/v2/posts", requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
-        }
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+            console.log('new post is added!');
+        })
     </script>
 </section>
 <?php get_footer(); ?>
