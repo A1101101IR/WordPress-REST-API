37f638356db49cf1df056c3820a4fa652a6197bf
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-26 17:47:07 +0100

    Added delete function

--- template-external.php
+++ template-external.php
@@ -5,10 +5,11 @@ Template Name: External data
 ?>
 <?php get_header(); ?>
 <section class="content-section">
-    <h2>News</h2>
+    <h2>Add Post!</h2>
     <div id="post"></div>
     <script>
         let postURL = 'http://localhost:8000/posts';
+        /* get function */
         async function getPosts() {
             let postData = await fetch(postURL);
             let postJson = await postData.json();
@@ -20,11 +21,24 @@ Template Name: External data
                     `<div class="post-body">
                         <h1 class="post-title">${item.title}</h1>
                         <p class="post-caption">${item.body}</p>
-                        <a class="post-link">Link</a>
+                        <button onClick=deletePost(${item.id})>Delete</button>
                     </div>`;
                 });
             }
             document.getElementById('post').innerHTML = post;
+        }
+        /* Delete function */
+        function deletePost(id) {
+            console.log(id)
+            var formdata = new FormData();
+            var requestOptions = {
+            method: 'DELETE',
+            redirect: 'follow'
+            };
+            fetch(`http://localhost:8000/posts/${id}`, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log("item number " + id + " was deleted!"))
+            .catch(error => console.log('error', error));
         }
         getPosts();
     </script>
