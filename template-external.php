8bc3f831c4caba467b06bf63f1929813747a8ee2
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-26 20:22:35 +0100

    created addpost function

--- template-external.php
+++ template-external.php
@@ -6,17 +6,25 @@ Template Name: External data
 <?php get_header(); ?>
 <section class="content-section">
     <h2>Add Post!</h2>
+    <div>
+        <form id='myForm' action="">
+        <input type="text" name="title" id="title" placeholder="title" class="text-input title">
+        <textarea id="content" name="body" rows="8" cols="80" class="text-input" placeholder="body"></textarea>
+        <button type="submit">Add New Post</button>
+        </form>
+    </div>
     <div id="post"></div>
     <script>
-        let postURL = 'http://localhost:8000/posts';
-        /* get function */
+
+
+        /* This will print each post in our array/databas to frontend. */
         async function getPosts() {
-            let postData = await fetch(postURL);
+            let postData = await fetch('http://localhost:8000/posts');
             let postJson = await postData.json();
             let post = "";
             console.log(postJson);
             if (postJson) {
-                postJson.forEach(item => {
+                postJson.slice().reverse().forEach(item => {
                     post += 
                     `<div class="post-body">
                         <h1 class="post-title">${item.title}</h1>
@@ -27,9 +35,11 @@ Template Name: External data
             }
             document.getElementById('post').innerHTML = post;
         }
+
+
         /* Delete function */
+        /* It will delete the post and reload our page after 1/sec in order to update our post list in frontend. */
         function deletePost(id) {
-            console.log(id)
             var formdata = new FormData();
             var requestOptions = {
             method: 'DELETE',
@@ -39,7 +49,41 @@ Template Name: External data
             .then(response => response.text())
             .then(result => console.log("item number " + id + " was deleted!"))
             .catch(error => console.log('error', error));
+            setTimeout(() => {
+                location.reload(); 
+            }, 1000);
+            console.log("reload!")
         }
+
+
+        
+        /* Function for add post */
+        /* When form is submited it will make a post req and reload our page after 1/sec in order to update our post list in frontend.*/
+        const myForm = document.getElementById('myForm');
+        myForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+            const formData = new FormData(this);
+            const input = new URLSearchParams();
+            for (const pair of formData) {
+                input.append(pair[0], pair[1],)
+            };
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: input,
+            redirect: 'follow'
+            };
+            fetch("http://localhost:8000/posts", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+            setTimeout(() => {
+                location.reload(); 
+                console.log("reload!")
+            }, 1000);
+        })
         getPosts();
     </script>
 </section>
