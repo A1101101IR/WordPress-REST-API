20b821b43a1f89612d0237d75e77a439bdf0dcbb
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-28 20:34:51 +0200

    created put function

--- single.php
+++ single.php
@@ -59,21 +59,51 @@
           let postData = await fetch("http://wordpress.local/wp-json/wp/v2/posts/" + $post_id, requestOptions);
           let postJson = await postData.json();
           console.log(postJson);
-          /* let post = '';
+          let post = '';
           if (postJson) {
-              
               post += 
                       `
-                      <form id='addPost' action="">
+                      <form id='putPost' action="">
                       <h1></h1>
-                      <input type="text" name="title" id="title" placeholder="title" class="text-input title">
-                      <textarea id="content" name="body" rows="8" cols="80" class="text-input" placeholder="body"></textarea>
+                      <input type="text" name="title" id="title" value="${postJson.title.rendered}" class="text-input title">
+                      <input type="text" name="content" id="title" value="${postJson.content.rendered}" class="text-input title">
                       <button type="submit">Update post</button>
+                      </form>
                       `;
               }
-            document.getElementById('editFormDiv').innerHTML = post; */
+            document.getElementById('editFormDiv').innerHTML = post;
+            const putPost = document.getElementById('putPost');        
+            putPost.addEventListener('submit', function (e) {
+                e.preventDefault();
+                const formData = new FormData(this);
+                const input = new URLSearchParams();
+                for (const pair of formData) {
+                    input.append(pair[0], pair[1],)
+                };
+
+                var myHeaders = new Headers();
+                myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC93b3JkcHJlc3MubG9jYWwiLCJpYXQiOjE2NDgxMjMwOTAsIm5iZiI6MTY0ODEyMzA5MCwiZXhwIjoxNjQ4NzI3ODkwLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.haL4mOLTzCaLRXPbmW6YrUrCmbpzfalRYMIcEdY6bsU");
+                myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+                var requestOptions = {
+                  method: 'PUT',
+                  headers: myHeaders,
+                  body: input,
+                  redirect: 'follow'
+                };
+                fetch("http://wordpress.local/wp-json/wp/v2/posts/188", requestOptions)
+                .then(res => res.text())
+                .then(res => console.log(res))
+                .catch(error => console.log(error));
+                setTimeout(() => {
+                    window.location='/';
+                    console.log("reload!")
+                }, 300);
+            })
+            
         }
 
+        
         
       </script>
 
